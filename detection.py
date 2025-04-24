e6eeb1fd6f2d0d43e8ea04c66843ade220b75015
williamxieigs <yi.xie@igsl-group.com>
2025-04-24 17:34:53 +0800

    update

--- detection.py
+++ detection.py
@@ -6,24 +6,20 @@ from langchain_core.output_parsers import JsonOutputParser
 from models import Qwen25VL7B_dashscope
 from configs import dashscope_api_key
 
-class Target(BaseModel):
-  x: int = Field(description = 'the upper left x coordinate of a target bouding box.')
-  y: int = Field(description = 'the upper left y coordinate of a target bounding box.')
-  width: int = Field(description = 'the width of a target bounding box.')
-  height: int = Field(description = 'the height of a target bounding box.')
+class BoundingBox(BaseModel):
+  bbox_2d: List[int] = Field(description = '[x1, y1, x2, y2]'),
+  label: str = Field(description = 'type of object')
 
 class DetRes(BaseModel):
-  targets: List[Target] = Field(description = 'a list of Target object which represents all detected targets.')
+  targets: List[BoundingBox] = Field(description = 'a list of target bounding boxes.')
 
 class Detection(object):
   def __init__(self,):
     self.qwen = Qwen25VL7B_dashscope(dashscope_api_key)
     self.parser = JsonOutputParser(pydantic_object = DetRes)
     self.instruction = self.parser.get_format_instructions()
-  def detect(self, image, object_type = 'people'):
-    prompt = f"""please detect objects of type {object_type} and output in the following format:
-
-{self.instruction}"""
+  def detect(self, image, target_type = None):
+    prompt = f"""Otline the position of each {target_type} object and output all the coordinates in JSON format."""
     result = self.qwen.inference(prompt, image)
     outputs = self.parser.parse(result)
     return outputs
@@ -33,5 +29,9 @@ if __name__ == "__main__":
   img = cv2.imread('test.jpg')
   det = Detection()
   results = det.detect(img)
-  import pdb; pdb.set_trace()
+  print(results)
+  for result in results:
+    x1, y1, x2, y2 = result
+    img = cv2.rectangle(img, (x1, y1), (x2, y2), (0,255,0), 2)
+  cv2.imwrite('output.png', img)
 
